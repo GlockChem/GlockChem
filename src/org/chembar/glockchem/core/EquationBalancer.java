a9f780187c88554756771621dfedc5e231a79254
EAirPeter <VioletCrestfall@hotmail.com>
2016-10-02 14:31:39 +0900

    Added Gaussian Elimination.

--- src/org/chembar/glockchem/core/EquationBalancer.java
+++ src/org/chembar/glockchem/core/EquationBalancer.java
@@ -109,9 +109,88 @@ public class EquationBalancer {
 		
 		// 建立矩阵对象
 		Matrix mat = new Matrix(cols, lines);
+		int[][] mtx = mat.matrix;
 		
+		// Gaussian Elimination
+		int K = 0;
+		int[] pos = new int[lines];
+		for (int i = 0; i < cols; ++i) {
+		    if (mtx[K][i] == 0) {
+		        int u = K;
+		        while (u < lines && mtx[u][i] == 0)
+		            ++u;
+		        if (u < lines)
+		            lnSwap(mtx[K], mtx[u], i);
+		        else
+		            continue;
+		    }
+		    pos[K] = i;
+		    lnSimplify(mtx[K], i);
+		    for (int j = K + 1; j < lines; ++j)
+		        if (mtx[j][i] != 0)
+		            lnSubstract(mtx[j], mtx[K], i, i);
+		    ++K;
+		}
+		for (int i = 1; i < K; ++i) {
+		    lnSimplify(mtx[i], pos[i]);
+		    for (int j = 0; j < i; ++j)
+		        lnSubstract(mtx[j], mtx[i], pos[j], pos[i]);
+		}
+		for (int i = 0; i < K; ++i)
+		    lnSimplify(mtx[i], pos[i]);
 		return null;
 	}
 	
+	private static void lnSubstract(int[] ln1, int ln2[], int pos, int key) {
+	    // Assert: ln1[pos] != 0 && ln2[pos] != 0
+	    int d = gcd(ln1[key], ln2[key]);
+	    int a1 = Math.abs(ln2[key]) / d;
+        int a2 = Math.abs(ln1[key]) / d;
+	    if (ln1[key] * ln2[key] > 0)
+    	    for (int i = pos; i < ln1.length; ++i)
+    	        ln1[i] = ln1[i] * a1 - ln2[i] * a2;
+	    else
+            for (int i = pos; i < ln1.length; ++i)
+                ln1[i] = ln1[i] * a1 + ln2[i] * a2;
+	}
+	
+	private static void lnSimplify(int[] ln, int pos) {
+	    // Assert: ln[pos] != 0
+	    int d = lnGcd(ln, pos);
+	    if (d > 1)
+	        for (int i = pos; i < ln.length; ++i)
+	            ln[i] /= d;
+	    if (ln[pos] < 0)
+	        for (int i = pos; i < ln.length; ++i)
+	            ln[i] = -ln[i];
+	}
+	
+	private static int lnGcd(int[] ln, int pos) {
+        int d = 0;
+        for (int i = pos; d != 1 && i < ln.length; ++i)
+            d = gcd(d, ln[i]);
+        return d;
+    }
+	
+	private static void lnSwap(int[] ln1, int[] ln2, int pos) {
+	    for (int i = pos; i < ln1.length; ++i) {
+	        int tmp = ln1[i];
+	        ln1[i] = ln2[i];
+	        ln2[i] = tmp;
+	    }
+	}
+	
+	private static int gcd(int a, int b) {
+        if (a < 0)
+            a = -a;
+        if (b < 0)
+            b = -b;
+        while (b != 0) {
+            int c = a % b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
 	
 }
--- src/org/chembar/glockchem/core/Matrix.java
+++ src/org/chembar/glockchem/core/Matrix.java
@@ -43,12 +43,11 @@ public final class Matrix {
 		return strTemp;
 	}
 	
-	public final void swapLines(int ln1, int ln2) {
-		int[] lnTemp = this.matrix[ln1].clone();
-		
-		for (int i=0; i<this.matrix[0].length; i++) {
-			this.matrix[ln1][i] = this.matrix[ln2][i];
-			this.matrix[ln2][i] = lnTemp[i];
-		}
+	public final void swapLines(int ln1, int ln2, int pos) {
+	    for (int i = pos; i < matrix[ln1].length; ++i) {
+	        int tmp = matrix[ln1][i];
+	        matrix[ln1][i] = matrix[ln2][i];
+	        matrix[ln2][i] = tmp;
+	    }
 	}
 }
